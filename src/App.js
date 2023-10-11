8ff0aaa7fe311af5cb71feaf61debc55d27ef4d4
TatiaSalukvadze <tatiasaluqvadze86@gmail.com>
2023-10-11 12:40:57 +0400

    adding react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import pic from "./images/image-equilibrium.jpg";
+import av from "./images/image-avatar.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={pic} className="pic" />
+      <h3>Equlibrium #4872</h3>
+      <p id="lorem">Our Equilibrium collection promotes balance and calm.</p>
+      <div id="float">
+        <span id="eth">&#11030; 0.041 ETH</span>
+        <span id="left">&#128337; 3 days left</span>
+      </div>
+      <Bot name="Jules Wyvern" />
+    </div>
+  );
+}
+
+function Bot(props) {
+  //props aris object, e can write {name} parametrad
+  return (
+    <div id="botom">
+      <img src={av} />
+      <span>
+        created by <b>{props.name}</b>
+      </span>
     </div>
   );
 }
